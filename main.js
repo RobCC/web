4ac08d051363f9c4a2856d2f43706fb69c86b871
José Roberto Chávez Cobián <rrc0138@gmail.com>
2018-04-07 13:27:13 -0500

    Test

--- main.js
+++ main.js
@@ -34,6 +34,6 @@ requirejs.config({
 //define does
 requirejs(['jquery', 'underscore', 'backbone', './js/app'], function($, _, Backbone, App) {
   var app;
-  console.log("Hello from requireJS1");
+  console.log("Hello from requireJS12");
   return app = new App();
 });
