04bbf6d3e6471be71cb34ce40287796d60b354cb
José Roberto Chávez Cobián <rrc0138@gmail.com>
2018-04-08 23:39:33 -0500

    html changes

--- js/app.js
+++ js/app.js
@@ -2,13 +2,13 @@ define(['jquery', 'underscore', 'backbone', 'text!../html/app.html'], function($
   return Backbone.View.extend({
     id: 'main-container',
     initialize: function() {
-      console.log("Hello from Backbone!");
       return this.render();
     },
     render: function() {
       var $body;
       $body = $('body');
-      return $body.append(Template);
+      this.$el.html(Template);
+      return $body.append(this.$el);
     }
   });
 });
