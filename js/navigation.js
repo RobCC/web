025d9160140a460deb6ecaec7cf31abe69befef3
José Roberto Chávez Cobián <rrc0138@gmail.com>
2018-04-15 16:44:13 -0500

    css update

--- js/navigation.js
+++ js/navigation.js
@@ -1,12 +1,24 @@
 define(['jquery', 'underscore', 'backbone', 'materialize', 'text!../html/navigation.html'], function($, _, Backbone, Materialize, Template) {
   return Backbone.View.extend({
     tagName: 'nav',
+    events: {
+      'click .option': 'goToSection'
+    },
     initialize: function() {
       return this.render();
     },
     render: function() {
       this.$el.html(Template);
       return this;
+    },
+    animate: function() {
+      return this.$el.animateCss('fadeInUp');
+    },
+    goToSection: function(e) {
+      var $target;
+      $target = $(e.currentTarget);
+      this.$('.col.active').removeClass('active');
+      return $target.parent().addClass('active');
     }
   });
 });
