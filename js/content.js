7e0bfcecba4f7fec22b8cb0654dc971e8ba0e0cb
José Roberto Chávez Cobián <rrc0138@gmail.com>
2018-04-26 23:57:12 -0500

    Fix double-rendering issue. Added stick nav fn.

--- js/content.js
+++ js/content.js
@@ -1,9 +1,7 @@
 define(['jquery', 'underscore', 'backbone', 'materialize', 'text!../html/content.html'], function($, _, Backbone, Materialize, Template) {
   return Backbone.View.extend({
     className: 'content-wrapper',
-    initialize: function() {
-      return this.render();
-    },
+    initialize: function() {},
     render: function() {
       this.$el.html(Template);
       return this;
