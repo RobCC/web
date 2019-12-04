7e8e76abe1cda37fc136a41f5e69e232a3522073
José Roberto Chávez Cobián <rrc0138@gmail.com>
2019-12-03 23:40:36 -0600

    Remove unused components

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,11 +1,8 @@
 import React from 'react';
 
-import Input from '../../components/Input/Input';
-
 const Home = () => (
   <div>
     Home page.
-    <Input inputType="textarea" type="text" name="test" placeholder="HELP" />
   </div>
 );
 
