92004172999f0cf498ae2a52086caf36fa467f75
José Roberto Chávez Cobián <rrc0138@gmail.com>
2019-12-08 15:09:15 -0600

    Refactor email classes

--- src/views/Email/Email.js
+++ src/views/Email/Email.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import classNames from 'classnames';
 
 import { email } from '#/store/ducks';
 import styles from './email.scss';
 
 const Email = () => {
   const isEmailOpen = useSelector((state) => email.isEmailOpen(state));
+  const emailClasses = classNames(styles.email, { [styles.active]: isEmailOpen });
 
   return (
-    <div className={styles.email} style={{ width: isEmailOpen ? 200 : 0 }}>Help me</div>
+    <div className={emailClasses}>Help me</div>
   );
 };
 
-export default Email;
+export default React.memo(Email);
