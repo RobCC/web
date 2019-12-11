d18ce386066f7ce8abd57726b5d5aa0ad5a3d1d5
José Roberto Chávez Cobián <rrc0138@gmail.com>
2019-12-10 22:26:11 -0600

    Add lottie and example

--- src/views/Resume/Resume.js
+++ src/views/Resume/Resume.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
+import lottie from 'lottie-web';
 
-const Resume = () => (
-  <div>Help me</div>
-);
+import bouncingBall from '~/public/ae/ball.json';
+
+import styles from './resume.scss';
+
+const Resume = () => {
+  const ballRef = useRef(null);
+
+  useEffect(() => {
+    lottie.loadAnimation({
+      container: ballRef.current,
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      animationData: bouncingBall,
+    });
+  }, []);
+
+  return (
+    <div className={styles.wip} ref={ballRef}>
+      <div>Work in progress!</div>
+    </div>
+  );
+};
 
 export default Resume;
