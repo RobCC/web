b898ed6a948a71f1ac9913d37ec99ce1dffeb2f0
José Roberto Chávez Cobián <rrc0138@gmail.com>
2020-06-16 23:57:26 -0500

    Tagged template example

--- src/utils/content.js
+++ src/utils/content.js
@@ -1,6 +1,6 @@
-import getRandomElement from '#/utils/getRandomElement';
 import lines from '#/codeLines';
 
+const getRandomElement = (items) => items[Math.floor(Math.random() * items.length)];
 const getRandomGreet = (greets) => getRandomElement(Object.keys(greets).map((k) => greets[k]));
 
 export const tabList = {
@@ -8,8 +8,17 @@ export const tabList = {
   contact: 'contact',
 };
 
+// TODO: Refactor
+const getCodeLines = ([string]) => string.split('\n').slice(1, -1);
+
+const home = getCodeLines`
+Hi! My name's Roberto. I'm a $(green, web) developer, living in Guadalajara, México.
+
+Currently working at $[Accedo,https://www.accedo.tv/]. // As a web developer
+`;
+
 export const tabMap = {
-  [tabList.greet]: [...getRandomGreet(lines.greets), ...lines.home],
+  [tabList.greet]: [...getRandomGreet(lines.greets), ...home],
   [tabList.contact]: lines.contact,
 };
 
--- src/utils/getRandomElement.js
+++
@@ -1,3 +0,0 @@
-const getRandomElement = (items) => items[Math.floor(Math.random() * items.length)];
-
-export default getRandomElement;
