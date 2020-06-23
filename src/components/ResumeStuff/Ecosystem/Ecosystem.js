4c780ddc8ef13e51b47c898bafaf6f05a0be00c2
José Roberto Chávez Cobián <rrc0138@gmail.com>
2020-06-22 23:14:21 -0500

    Add ecosystem

--- src/components/ResumeStuff/Ecosystem/Ecosystem.js
+++ src/components/ResumeStuff/Ecosystem/Ecosystem.js
@@ -1,25 +1,63 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faJs, faNodeJs } from '@fortawesome/free-brands-svg-icons';
+import {
+  faJs, faNodeJs, faReact, faSass, faNpm,
+} from '@fortawesome/free-brands-svg-icons';
 
 import VsCode from '#/svg/VsCode';
 import Typescript from '#/svg/Typescript';
+import Webpack from '#/svg/Webpack';
+import Babel from '#/svg/Babel';
+import Redux from '#/svg/Redux';
 import Section from '../Section/Section';
 
 import styles from './ecosystem.scss';
 
+import rollup from '~/public/images/rollup.png';
+
+function setStyles(c) {
+  return classNames(styles.element, c);
+}
+
 /* eslint-disable max-len */
 const Ecosystem = ({ onScreen }) => (
   <Section className={styles.wrapper} title="Ecosystem">
-    <div className={styles.element}>
-      <VsCode />
-    </div>
-    <div className={styles.element}>
-      <FontAwesomeIcon icon={faJs} />
-    </div>
-    <div className={styles.element}>
-      <FontAwesomeIcon icon={faNodeJs} />
+    <div className={styles.table}>
+      <div className={setStyles(styles.vscode)}>
+        <VsCode />
+      </div>
+      <div className={setStyles(styles.ts)}>
+        <Typescript />
+      </div>
+      <div className={setStyles(styles.js)}>
+        <FontAwesomeIcon icon={faJs} />
+      </div>
+      <div className={setStyles(styles.node)}>
+        <FontAwesomeIcon icon={faNodeJs} />
+      </div>
+      <div className={setStyles(styles.react)}>
+        <FontAwesomeIcon icon={faReact} />
+      </div>
+      <div className={setStyles(styles.redux)}>
+        <Redux />
+      </div>
+      <div className={setStyles(styles.sass)}>
+        <FontAwesomeIcon icon={faSass} />
+      </div>
+      <div className={setStyles(styles.npm)}>
+        <FontAwesomeIcon icon={faNpm} />
+      </div>
+      <div className={setStyles(styles.babel)}>
+        <Babel />
+      </div>
+      <div className={setStyles(styles.webpack)}>
+        <Webpack />
+      </div>
+      <div className={styles.element}>
+        <img alt="Rollup" src={rollup} />
+      </div>
     </div>
   </Section>
 );
