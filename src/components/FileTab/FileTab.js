43e18dce1de1cde632e4711903ca952247f9a86c
José Roberto Chávez Cobián <rrc0138@gmail.com>
2020-06-28 23:26:22 -0500

    Add closeTab

--- src/components/FileTab/FileTab.js
+++ src/components/FileTab/FileTab.js
@@ -1,9 +1,11 @@
-import React, { useCallback } from 'react';
+import React, { useState, useCallback } from 'react';
 import PropTypes from 'prop-types';
 import { useSelector, useDispatch } from 'react-redux';
 import classNames from 'classnames';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
-import { getCurrentFile, changeFile } from '#/store/ducks/file';
+import { getCurrentFile, changeFile, closeFile } from '#/store/ducks/file';
 import { getFileName } from '#/_files';
 
 import styles from './fileTab.scss';
@@ -15,24 +17,52 @@ function getTabStyles(isCurrentTab) {
 }
 
 const FileTab = ({ icon = '', name }) => {
+  const [showClose, setShowClose] = useState(false);
   const dispatch = useDispatch();
   const currentTab = useSelector((store) => getCurrentFile(store));
   const tabClasses = getTabStyles(name === currentTab);
+
+  const onMouseEnter = useCallback(() => setShowClose(true, []));
+  const onMouseLeave = useCallback(() => setShowClose(false, []));
+  const closeTab = useCallback((e) => {
+    e.stopPropagation();
+
+    if (!showClose) {
+      return;
+    }
+
+    dispatch(closeFile(name));
+  }, [showClose, name]);
+
+  const changeCurrentTab = useCallback(() => {
+    dispatch(changeFile(name));
+  }, [name]);
 
   const iconClasses = classNames({
     [styles.js]: icon && icon === 'JS',
     [styles.css]: icon && icon === '#',
   });
 
-  const changeCurrentTab = useCallback(() => {
-    dispatch(changeFile(name));
-  }, [name]);
+  const closeClasses = classNames(styles.close, {
+    [styles.show]: showClose,
+  });
 
   return (
-    <button type="button" className={tabClasses} onClick={changeCurrentTab}>
+    <div
+      role="button"
+      tabIndex={0}
+      className={tabClasses}
+      onClick={changeCurrentTab}
+      onKeyDown={changeCurrentTab}
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+    >
+      <button type="button" className={closeClasses} onClick={closeTab}>
+        <FontAwesomeIcon icon={faTimes} />
+      </button>
       {icon && <span className={iconClasses}>{icon}</span>}
       {getFileName(name)}
-    </button>
+    </div>
   );
 };
 
