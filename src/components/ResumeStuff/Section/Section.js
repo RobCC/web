820edb92ffe0a3ea5817a492c99edb183000e7a9
José Roberto Chávez Cobián <rrc0138@gmail.com>
2020-07-04 20:13:32 -0500

    Clear TODO

--- src/components/ResumeStuff/Section/Section.js
+++ src/components/ResumeStuff/Section/Section.js
@@ -6,24 +6,15 @@ import styles from './section.scss';
 
 /* eslint-disable max-len */
 const Section = ({
-  style, className = '', title, children,
+  className = '', title, children,
 }) => (
-  <div style={style} className={classNames(styles.section, className)}>
+  <div className={classNames(styles.section, className)}>
     <div className={styles.title}>{title}</div>
     {children}
   </div>
 );
 
-// TODO: Remove style
-/* eslint-disable react/forbid-prop-types */
 Section.propTypes = {
-  style: PropTypes.shape({
-    height: PropTypes.string,
-    flex: PropTypes.string,
-    left: PropTypes.string,
-    right: PropTypes.string,
-    width: PropTypes.string,
-  }),
   className: PropTypes.string,
   title: PropTypes.string,
   children: PropTypes.node,
