010b87a33128cd73e7503106d98644cddae37341
RobCC <rrc0138@gmail.com>
2024-02-10 02:23:55 -0600

    refactor: App

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,14 +3,12 @@ import { Outlet } from 'react-router-dom';
 import ActivityBar from '#/components/ActivityBar/ActivityBar';
 import SideBar from '#/components/SideBar/SideBar';
 
-import styles from './app.scss';
-
 export default function App() {
   return (
-    <div className={styles.container}>
+    <>
       <ActivityBar />
       <SideBar />
       <Outlet />
-    </div>
+    </>
   );
 }
