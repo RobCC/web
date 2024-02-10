e2ebaa7fcf8556e425bca22666cd1e92266fb609
RobCC <rrc0138@gmail.com>
2024-02-10 02:21:52 -0600

    refactor: activity bar

--- src/components/ActivityBar/ActivityBar.tsx
+++ src/components/ActivityBar/ActivityBar.tsx
@@ -1,7 +1,7 @@
 import { IconExplorer } from '#/components/Icones';
 import { explorer } from '#/store';
 
-import Item from './ActivityBarItem';
+import ActivityBarItem from './ActivityBarItem';
 import styles from './activityBar.scss';
 
 const { toggleSideBar } = explorer;
@@ -9,9 +9,9 @@ const { toggleSideBar } = explorer;
 export default function ActivityBar() {
   return (
     <div className={styles.bar}>
-      <Item label="Explorer" onClick={toggleSideBar} isActive>
+      <ActivityBarItem label="Explorer" onClick={toggleSideBar} isActive>
         <IconExplorer />
-      </Item>
+      </ActivityBarItem>
     </div>
   );
 }
