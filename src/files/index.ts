fd6d1e0b201a15d93130519cb9ac1c6bfc70c4ef
RobCC <rrc0138@gmail.com>
2025-05-25 13:22:37 -0600

    remove test files

--- src/files/index.ts
+++ src/files/index.ts
@@ -2,7 +2,7 @@ import { folderUtils } from '#/utils/directory';
 
 import projects from './projects';
 import blog from './blog';
-import test from './test';
+// import test from './test';
 import readme from './readme';
 import contact from './contact';
 import Settings from './_Settings/Settings';
@@ -11,7 +11,7 @@ import Resume from './_Resume/Resume';
 export default folderUtils.create('', [
   contact,
   readme,
-  test,
+  // test,
   blog,
   projects,
   Settings,
