eeb9e0fa75d9ec924cf3c7ffc7bff55da5b3932a
RobCC <rrc0138@gmail.com>
2026-03-14 20:58:08 -0600

    chore: out with the old

--- stylelint.config.js
+++
@@ -1,9 +0,0 @@
-export default {
-  extends: ['stylelint-config-standard-scss', 'stylelint-prettier/recommended'],
-  plugins: ['stylelint-order'],
-  overrides: [
-    {
-      files: ['**/*.css'],
-    },
-  ],
-};
