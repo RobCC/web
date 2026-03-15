c7eb732abe7f2f8aaa60939c2a48f9f95d56d5f1
RobCC <rrc0138@gmail.com>
2026-03-14 21:47:43 -0600

    chore: remove unused

--- src/svg/index.js
+++
@@ -1,5 +0,0 @@
-export { default as VsCode } from './VsCode';
-export { default as Typescript } from './Typescript';
-export { default as Webpack } from './Webpack';
-export { default as Babel } from './Babel';
-export { default as Redux } from './Redux';
